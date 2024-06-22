3424159c1f536203591b64d1cc569ea24bb4fecf
GustavoPizente <gustavonazarine@gmail.com>
2024-06-21 21:06:47 -0300

    Cadastrar pronto, falta o Buscar

--- src/Formulario.js
+++ src/Formulario.js
@@ -1,36 +1,49 @@
 import React, { useState } from "react";
 import Resultado from "./Resultado";
 import SvgPesquisar from "./SvgPesquisar";
+import axios from "axios";
 
 function Formulario() {
   // Defina os estados para os campos do formulário
-  const [madeira, setMadeira] = useState("");
+  const [nomeMadeira, setNomeMadeira] = useState("");
   const [cor, setCor] = useState("");
   const [localidade, setLocalidade] = useState("");
-
-  // Coloca esse resultado na props após o submit
-
-  const [confirmedMadeira, setConfirmedMadeira] = useState("");
-  const [confirmedCor, setConfirmedCor] = useState("");
-  const [confirmedLocalidade, setConfirmedlocalidade] = useState("");
 
   // Define o estado do componente Resultado
   const [resultadoIsOpen, setResultadoIsOpen] = useState(false);
 
   // Função para lidar com a submissão do formulário
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setResultadoIsOpen(true);
-    setConfirmedMadeira(madeira);
-    setConfirmedCor(cor);
-    setConfirmedlocalidade(localidade);
+    const madeira = {
+      name: nomeMadeira,
+      cor: cor,
+      localidade : localidade,
+    };
+    console.log(madeira);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/madeirasbusca",
+        { madeira },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      console.log("Madeira:", nomeMadeira);
+      console.log("Cor:", cor);
+      console.log("Localidade:", localidade);
+      
+      console.log(response.data);
+    } catch (error) {
+      console.error("Erro ao enviar requisição:", error);
+    }
 
     // Aqui você pode fazer algo com os dados do formulário
-
-    console.log(resultadoIsOpen);
-    console.log("Madeira:", madeira);
-    console.log("Cor:", cor);
-    console.log("Localidade:", localidade);
   };
 
   return (
@@ -38,11 +51,13 @@ function Formulario() {
       <form onSubmit={handleSubmit} className="formulariopesquisar">
         <div className="entradaformulario">
           <label className="itensformulariopesquisar">
-            Madeira:
+            Nome:
             <input
+              name="madeira"
               type="text"
-              value={madeira}
-              onChange={(e) => setMadeira(e.target.value)}
+              value={nomeMadeira}
+              onChange={(e) => setNomeMadeira(e.target.value)}
+              
             />
           </label>
         </div>
@@ -70,12 +85,6 @@ function Formulario() {
           <SvgPesquisar></SvgPesquisar>
         </button>
       </form>
-      <Resultado
-        isOpen={resultadoIsOpen}
-        madeira={confirmedMadeira}
-        cor={confirmedCor}
-        localidade={confirmedLocalidade}
-      ></Resultado>
     </div>
   );
 }
---
+++ src/formularioCadastro.js
@@ -0,0 +1,134 @@
+import { useState } from "react";
+import axios from "axios";
+import Modal from "react-modal";
+import SvgBtnSalvar from "./SvgBtnSalvar";
+
+function FormularioCadastro() {
+  // Defina os estados para os campos do formulário
+
+  const estadoinicial = " ";
+
+  const [nomeMadeira, setNomeMadeira] = useState(" ");
+  const [cor, setCor] = useState(" ");
+  const [localidade, setLocalidade] = useState(" ");
+
+  // Define o estado do componente Agradecimento
+  const [AgradecimentoIsOpen, setAgradecimentoIsOpen] = useState(false);
+
+  const closeAgradecimento = () => {
+    setAgradecimentoIsOpen(false);
+  };
+
+  // Função para lidar com a submissão do formulário
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (
+      nomeMadeira === estadoinicial ||
+      cor === estadoinicial ||
+      localidade === estadoinicial
+    ) {
+      alert("Você precisa preencher todos os campos");
+
+      return;
+    } else {
+      setAgradecimentoIsOpen(true);
+
+      setNomeMadeira(" ");
+      setCor(" ");
+      setLocalidade(" ");
+      const madeira = {
+        name: nomeMadeira,
+        cor: cor,
+        localidade: localidade,
+      };
+      console.log(madeira);
+
+      try {
+        const response = await axios.post(
+          "http://localhost:5000/madeiras",
+          { madeira },
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        console.log("Madeira:", nomeMadeira);
+        console.log("Cor:", cor);
+        console.log("Localidade:", localidade);
+
+        console.log(response.data);
+      } catch (error) {
+        console.error("Erro ao enviar requisição:", error);
+      }
+    }
+  };
+
+  return (
+    <div className="conteudoformulario">
+      <form onSubmit={handleSubmit} className="formulariopesquisar">
+        <div className="entradaformulario">
+          <label className="itensformulariopesquisar">
+            Nome:
+            <input
+              name="madeira"
+              type="text"
+              value={nomeMadeira}
+              onChange={(e) => setNomeMadeira(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="entradaformulario">
+          <label className="itensformulariopesquisar">
+            <p>Cor:</p>
+            <input
+              type="text"
+              value={cor}
+              onChange={(e) => setCor(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className="entradaformulario">
+          <label className="itensformulariopesquisar">
+            Localidade:
+            <input
+              type="text"
+              value={localidade}
+              onChange={(e) => setLocalidade(e.target.value)}
+            />
+          </label>
+        </div>
+        <button type="submit" className="submitpesquisar">
+          <SvgBtnSalvar></SvgBtnSalvar>
+        </button>
+      </form>
+
+      <Modal
+        className="modalagradecimento"
+        isOpen={AgradecimentoIsOpen}
+        onRequestClose={closeAgradecimento}
+        contentLabel="ModalAgradecimento"
+      >
+        <div className="conteudomodalagradecimento">
+          <h1>
+            Obrigado por compartilhar seu conhecimento com outros marceneiros!
+          </h1>
+
+          <button
+            className="botaofecharagradecimento"
+            onClick={() => {
+              closeAgradecimento();
+            }}
+          >
+            Voltar
+          </button>
+        </div>
+      </Modal>
+    </div>
+  );
+}
+
+export default FormularioCadastro;
