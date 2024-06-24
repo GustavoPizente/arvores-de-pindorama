423954ca5a9de4da43589effc1efca19057994c4
GustavoPizente <gustavonazarine@gmail.com>
2024-06-24 15:08:28 -0300

    Footer e botoes ok

--- src/Footer.js
+++ src/Footer.js
@@ -1,15 +1,19 @@
+import SvgBtnWhatsapp from "./SvgBtnWhatsapp";
+import SvgGitHub from "./SvgGitHub";
+import SvgLogo from "./SvgLogo";
+
 function Footer() {
   return (
     <footer className="footer">
       <div className="conteudofooter">
-        <a href="http://www.google.com">
-          <div className="logo"></div>
+      <a className="linklogo"  href="https://www.linkedin.com/in/gustavo-pizente-nazarine-69b6812b7/">
+          <div className="logo"><SvgLogo></SvgLogo></div>
         </a>
-        <a href="http://www.google.com">
-          <div className="Contato">Contato</div>
+        <a className="linkcontato" href="https://wa.me/5514997627237?text=Fala%20a%C3%AD%2C%20Pindoramo!">
+          <div className="Contato"><SvgBtnWhatsapp></SvgBtnWhatsapp></div>
         </a>
-        <a href="http://www.google.com">
-          <div className="Sobre">Sobre</div>
+        <a className="sobrelink" href="https://github.com/GustavoPizente/arvores-de-pindorama">
+          <div className="Sobre"><SvgGitHub></SvgGitHub></div>
         </a>
       </div>
     </footer>
