aed3f45a3d75d6063aaa561870216d1d934db749
GustavoPizente <gustavonazarine@gmail.com>
2025-02-06 21:54:05 -0300

    agora com texto no modal banner2

--- src/Jogos.js
+++ src/Jogos.js
@@ -3,7 +3,7 @@ import React from "react";
 const Jogos = ({ isOpen, closeJogos }) => {
   return (
     <div className="jogos" style={{ display: isOpen ? "block" : "none" }}>
-      <h1>Olá, mundo!</h1>
+      <h1>Sabe dizer quais são esssas madeiras?</h1>
       <p>Este é um componente funcional.</p>
       <button onClick={closeJogos}>Fechar</button>
     </div>
