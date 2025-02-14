fcf19b77d2e4dfcad2fed9e79dfe0387bd3ba352
GustavoPizente <gustavonazarine@gmail.com>
2025-02-14 17:09:20 -0300

    2 questoes prontas

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,12 +1,12 @@
 import Pergunta1 from "./Sucupira.png"
-import Pergunta2 from "./mapa.png"
+import Pergunta2 from "./Roxinho.png"
 
 export const Perguntas = [
 
     { imagempergunta: Pergunta1,
      opcoesrespostas: [
-        {resposta: 'Jacarandá', correta: true, alternativa: 'A'},
-        {resposta: 'Sucupira', correta: false, alternativa: 'B'},
+        {resposta: 'Jacarandá', correta: false, alternativa: 'A'},
+        {resposta: 'Sucupira', correta: true, alternativa: 'B'},
         {resposta: 'Araucária', correta: false, alternativa: 'C'},
         {resposta: 'Jatobá', correta: false, alternativa: 'D'},
 
@@ -17,10 +17,10 @@ export const Perguntas = [
 
     { imagempergunta: Pergunta2,
         opcoesrespostas: [
-           {resposta: 'Jacarandá', correta: true, alternativa: 'A'},
-           {resposta: 'Sucupira', correta: false, alternativa: 'B'},
-           {resposta: 'Araucária', correta: false, alternativa: 'C'},
-           {resposta: 'Jatobá', correta: false, alternativa: 'D'},
+           {resposta: 'Sucupira', correta: false, alternativa: 'A'},
+           {resposta: 'Pau-Ferro', correta: false, alternativa: 'B'},
+           {resposta: 'Canela Preta', correta: false, alternativa: 'C'},
+           {resposta: 'Roxinho', correta: true, alternativa: 'D'},
    
    
         ]
