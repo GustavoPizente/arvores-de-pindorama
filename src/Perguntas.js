44719fe2c5648fcbe964e3a04f54193284907fb3
GustavoPizente <gustavonazarine@gmail.com>
2025-02-14 18:34:46 -0300

    agora com tres questoes

--- src/Perguntas.js
+++ src/Perguntas.js
@@ -1,30 +1,36 @@
-import Pergunta1 from "./Sucupira.png"
-import Pergunta2 from "./Roxinho.png"
+import Pergunta1 from "./Sucupira.png";
+import Pergunta2 from "./Roxinho.png";
+import Pergunta3 from "./Jaqueira.png"
 
 export const Perguntas = [
+  {
+    imagempergunta: Pergunta1,
+    opcoesrespostas: [
+      { resposta: "Jacarandá", correta: false, alternativa: "A" },
+      { resposta: "Sucupira", correta: true, alternativa: "B" },
+      { resposta: "Araucária", correta: false, alternativa: "C" },
+      { resposta: "Jatobá", correta: false, alternativa: "D" },
+    ],
+  },
+  {
+    imagempergunta: Pergunta2,
+    opcoesrespostas: [
+      { resposta: "Sucupira", correta: false, alternativa: "A" },
+      { resposta: "Pau-Ferro", correta: false, alternativa: "B" },
+      { resposta: "Canela Preta", correta: false, alternativa: "C" },
+      { resposta: "Roxinho", correta: true, alternativa: "D" },
+    ],
+  },
 
-    { imagempergunta: Pergunta1,
-     opcoesrespostas: [
-        {resposta: 'Jacarandá', correta: false, alternativa: 'A'},
-        {resposta: 'Sucupira', correta: true, alternativa: 'B'},
-        {resposta: 'Araucária', correta: false, alternativa: 'C'},
-        {resposta: 'Jatobá', correta: false, alternativa: 'D'},
+  {
+    imagempergunta: Pergunta3,
+    opcoesrespostas: [
+      { resposta: "Araucária", correta: false, alternativa: "A" },
+      { resposta: "Camboim", correta: false, alternativa: "B" },
+      { resposta: "Jaqueira", correta: true, alternativa: "C" },
+      { resposta: "Pinus", correta: false, alternativa: "D" },
+    ],
+  },
 
 
-     ]
-    }
-    ,
-
-    { imagempergunta: Pergunta2,
-        opcoesrespostas: [
-           {resposta: 'Sucupira', correta: false, alternativa: 'A'},
-           {resposta: 'Pau-Ferro', correta: false, alternativa: 'B'},
-           {resposta: 'Canela Preta', correta: false, alternativa: 'C'},
-           {resposta: 'Roxinho', correta: true, alternativa: 'D'},
-   
-   
-        ]
-       }
-
-       
-]
+];
