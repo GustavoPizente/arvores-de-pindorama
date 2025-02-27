2b24f4afc2cc2ff574a17d6a85422b57eaa073bc
GustavoPizente <gustavonazarine@gmail.com>
2025-02-27 20:08:51 -0300

    posicao do hero

--- src/Hero.js
+++ src/Hero.js
@@ -4,18 +4,18 @@ function Hero() {
   return (
 
     <div  className='parallax'>
-    <Parallax pages={2} style={{ top: '0', left: '0' }} className='animation'>
+    <Parallax pages={1.5} style={{ top: '0', left: '0' }} className='animation'>
       <ParallaxLayer offset={0} speed={2.5} id='fundomorro' className='animation_layer parallax'>
-        <p>Parallax</p>
+       
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={2.0}id='morro' className='animation_layer parallax'>
-        <p>Parallax</p>
+      <ParallaxLayer offset={0.5} speed={2.0}id='morro' className='animation_layer parallax'>
+       
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={0.5} id='floresta1' className='animation_layer parallax'>
-        <p>Parallax</p>
+      <ParallaxLayer offset={0.8} speed={0.5} id='floresta1' className='animation_layer parallax'>
+        
       </ParallaxLayer>
-      <ParallaxLayer offset={0} speed={1.5} id='floresta2' className='animation_layer parallax'>
-        <p>Parallax</p>
+      <ParallaxLayer offset={1.5} speed={1.5} id='floresta2' className='animation_layer parallax'>
+        
       </ParallaxLayer>
     </Parallax>
 
