293158b545c78ccaeefaa3073997e160f544acbb
GustavoPizente <gustavonazarine@gmail.com>
2025-03-05 16:04:43 -0300

    destaques ajustado

--- src/Destaques.js
+++ src/Destaques.js
@@ -41,11 +41,10 @@ function Destaques() {
 
         <div className="caixatextodestaques">
 
-            <h2 className="artigo"><img src={destaques1}/><div className="textodoartigo"><h2>Artigo 1</h2><h3>texto to artigo</h3></div></h2>
-            <h2 className="artigo"><img src={destaques2}/><div className="textodoartigo"><h2>Artigo 1</h2><h3>texto to artigo</h3></div></h2>
-            <h2 className="artigo"><img src={destaques3}/><div className="textodoartigo"><h2>Artigo 1</h2><h3>texto to artigo</h3></div></h2>
-            <h2 className="artigo"><img src={destaques4}/><div className="textodoartigo"><h2>Artigo 1</h2><h3>texto to artigo</h3></div></h2>
-            <h2 className="artigo"><img src={destaques5}/><div className="textodoartigo"><h2>Artigo 1</h2><h3>texto to artigo</h3></div></h2>
+            <h2 className="artigo"><img src={destaques1}/><div className="textodoartigo"><h2>Como Escolher o Melhor Alargador de Madeira</h2><h3>Descubra os tipos de alargadores e suas vantagens.</h3></div></h2>
+            <h2 className="artigo"><img src={destaques3}/><div className="textodoartigo"><h2>Passo a Passo para Construir uma Cadeira de Madeira</h2><h3>Guia simples para montar sua própria cadeira.</h3></div></h2>
+            <h2 className="artigo"><img src={destaques4}/><div className="textodoartigo"><h2>Brincos Artesanais de Madeira: Como Fazer e Vender</h2><h3> Aprenda a criar acessórios únicos em madeira.</h3></div></h2>
+            <h2 className="artigo"><img src={destaques5}/><div className="textodoartigo"><h2>Madeiras Vermelhas: Tipos e Aplicações na Marcenaria</h2><h3>Conheça as principais madeiras avermelhadas e seus usos.</h3></div></h2>
         </div>
     </div>
   );
